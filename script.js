b3cdcc0c6d401ce1184e04c5accfe4fe4fcdbd4e
Negin Rouhanizadeh <negin.rouhanizadeh@gmail.com>
2020-08-09 17:00:17 -0500

    completed the markup

---
+++ script.js
@@ -0,0 +1,24 @@
+
+
+var ApiKey = "b51e4fed81185bd10d4c7e74fba739ca"
+
+
+
+http://api.openweathermap.org/data/2.5/weather?q=Chicago&appid=b51e4fed81185bd10d4c7e74fba739ca
+
+
+// Good 5 day: 
+// http://api.openweathermap.org/data/2.5/forecast?q=chicago&appid=b51e4fed81185bd10d4c7e74fba739ca
+
+
+http://api.openweathermap.org/data/2.5/weather?q=Chicago&appid={b51e4fed81185bd10d4c7e74fba739ca
+
+
+response.list.main.temp
+
+
+response.list.main.humidity
+
+response.list.wind.speed
+UV Index
+
